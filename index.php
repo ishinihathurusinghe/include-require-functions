01be359b308c709fd26e29d403ff3715f924fb4c
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-05-15 11:08:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+ <?php
+    include('include/header.php');
+ ?>
+
+     <main>
+
+       <article>
+               <h1> Home Page </h1>
+               <p>  
+
+Paragraphs are the building blocks of papers. Without well-written paragraphs that flow logically from one idea to the next and that inform and help support in some meaningful way the central research problem being investigated, your paper will not be viewed as credible and, well, you'll probably receive a poor grade.
+
+Here are some suggestions for troubleshooting common problems associated with developing paragraphs:
+
+1.  The paragraph has no controlling idea. Imagine each paragraph as having three general layers of text. The core content is in the middle. It includes all the evidence you need to make the point. However, this evidence needs to be introduced by a topic sentence in some way or your readers don't know what to do with all the evidence you have given them. Therefore, the beginning of the paragraph explains the controlling idea of the paragraph. The last part of the paragraph tells the reader how the paragraph relates to the broader argument and often provides a transition to the next idea. Once you have mastered the use of topic sentences, you may decide that the topic sentence for a particular paragraph really should not be the first sentence of the paragraph. This is fine—the topic sentence can actually go at the beginning, middle, or end of a paragraph; what's important is that it is there to inform readers what the main idea of the paragraph is and how it relates back to the broader thesis of your paper.
+
+2.  The paragraph has more than one controlling idea. This is the most common reason why a paragraph is too long. If a paragraph is more than a page long, it likely contains more than one controlling idea. In this case, consider eliminating sentences that relate to the second idea, with the thought that maybe they don't really inform and help support the central research problem, or split the paragraph into two or more paragraphs, each with only one controlling idea.
+
+3.  Transitions are needed within the paragraph. You are probably familiar with the idea that transitions may be needed between paragraphs or sections in a paper. Sometimes they are also helpful within the body of a single paragraph. Within a paragraph, transitions are often single words or short phrases that help to establish relationships between ideas and to create a logical progression of those ideas in a paragraph. This is especially true within paragraphs that discuss multiple examples or discuss complex ideas, issues, or concepts.
+
+Arnaudet, Martin L. and Mary Ellen Barrett. Paragraph Development: A Guide for Students of English. 2nd edition. Englewood Cliffs, NJ: Prentice Hall Regents, 1990; Paragraph Development: Importance of Constructing Good Paragraphs. AP English Literature and Composition. Edublogs, 2012; Paragraphing. Centre for Applied Linguistics. University of Warwick.
+   </p>
+       </article>
+     </main>
+
+     <?php
+    include('include/footer.php');
+ ?>
+
+</body>
+
+
+</html>
---
+++ product.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+     <title> website title  </title>
+     <link rel= "stylesheet"  href="css/main.css">
+</head>
+
+<body>
+ 
+     <header>
+           <h1> website names </h1>
+      </header>
+
+      <nav>
+         <ui>
+            <li> <a href="index.php"> home </a> </li>
+
+            <li> <a href="about us.php"> about Us</a> </li>
+
+            <li> <a href="product.php"> product </a> </li>
+
+            <li> <a href="contact us.php"> contact Us </a> </li>
+
+         </ui>
+     </nav>
+
+     <main>
+
+       <article>
+               <h1> product </h1>
+               <p>  
+
+Paragraphs are the building blocks of papers. Without well-written paragraphs that flow logically from one idea to the next and that inform and help support in some meaningful way the central research problem being investigated, your paper will not be viewed as credible and, well, you'll probably receive a poor grade.
+
+Here are some suggestions for troubleshooting common problems associated with developing paragraphs:
+
+1.  The paragraph has no controlling idea. Imagine each paragraph as having three general layers of text. The core content is in the middle. It includes all the evidence you need to make the point. However, this evidence needs to be introduced by a topic sentence in some way or your readers don't know what to do with all the evidence you have given them. Therefore, the beginning of the paragraph explains the controlling idea of the paragraph. The last part of the paragraph tells the reader how the paragraph relates to the broader argument and often provides a transition to the next idea. Once you have mastered the use of topic sentences, you may decide that the topic sentence for a particular paragraph really should not be the first sentence of the paragraph. This is fine—the topic sentence can actually go at the beginning, middle, or end of a paragraph; what's important is that it is there to inform readers what the main idea of the paragraph is and how it relates back to the broader thesis of your paper.
+
+2.  The paragraph has more than one controlling idea. This is the most common reason why a paragraph is too long. If a paragraph is more than a page long, it likely contains more than one controlling idea. In this case, consider eliminating sentences that relate to the second idea, with the thought that maybe they don't really inform and help support the central research problem, or split the paragraph into two or more paragraphs, each with only one controlling idea.
+
+3.  Transitions are needed within the paragraph. You are probably familiar with the idea that transitions may be needed between paragraphs or sections in a paper. Sometimes they are also helpful within the body of a single paragraph. Within a paragraph, transitions are often single words or short phrases that help to establish relationships between ideas and to create a logical progression of those ideas in a paragraph. This is especially true within paragraphs that discuss multiple examples or discuss complex ideas, issues, or concepts.
+
+Arnaudet, Martin L. and Mary Ellen Barrett. Paragraph Development: A Guide for Students of English. 2nd edition. Englewood Cliffs, NJ: Prentice Hall Regents, 1990; Paragraph Development: Importance of Constructing Good Paragraphs. AP English Literature and Composition. Edublogs, 2012; Paragraphing. Centre for Applied Linguistics. University of Warwick.
+   </p>
+       </article>
+     </main>
+
+     <footer>
+           <p>  company name/ address/ tel- 555 555 5555 </p>
+     </footer>
+
+</body>
+
+
+</html>
